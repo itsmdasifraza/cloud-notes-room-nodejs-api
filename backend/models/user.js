4671ef13a38cb79233739caa150a999f78a94b0e
Md Asif Raza <itsmdasifraza@gmail.com>
2021-09-09 03:10:58 +0530

    (Backend) Register API Validation Improved
    Checking username or email already exist or not

--- backend/models/user.js
+++ backend/models/user.js
@@ -27,5 +27,4 @@ const userSchema = {
 }
 
 const userModel = mongoose.model('user', userSchema);
-userModel.createIndexes();
 module.exports = userModel;
--- backend/routes/register.js
+++ backend/routes/register.js
@@ -7,7 +7,7 @@ router.post('/',
     body('username','username must be minimum 3 character').isLength({ min: 3 }),
     body('email','email must be valid').isEmail(),
     body('password','password must be minimum 8 character').isLength({ min: 8 }),
-     function(req, res) {
+     async (req, res) => {
 
     const errors = validationResult(req);
 
@@ -20,11 +20,26 @@ router.post('/',
         email:req.body.email,
         password:req.body.password
     });
+
+     //check username already exist or not
+     let usernameExist = await userModel.findOne({username:req.body.username});
+     if(usernameExist){
+         return res.status(400).json({error:'request failed',
+             mssg:"username already in use"});
+     }
+
+    //check email already exist or not
+    let emailExist = await userModel.findOne({email:req.body.email});
+    if(emailExist){
+        return res.status(400).json({error:'request failed',
+            mssg:"email already in use"});
+    } 
+
     //pushing user object to mongo
     newUser.save(function(err,data){
         if(err) 
             return res.status(400).json({error:'request failed',
-                mssg:"username/email already in use"});
+                mssg:"something went wrong"});
         else{
            return res.status(200).json({success:'request success',
                         mssg:"congratulations user registered",
