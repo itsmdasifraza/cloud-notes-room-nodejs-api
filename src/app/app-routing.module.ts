965a6a5781a14d058bc0ab7a16c79c1885bb0d81
Md Asif Raza <itsmdasifraza@gmail.com>
2021-09-14 02:48:17 +0530

    (Frontend) Protected Routes With Guard => Authorized , Unauthorized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthorizedGuard } from './user/guard/authorized/authorized.guard';
+import { UnauthorizedGuard } from './user/guard/unauthorized/unauthorized.guard';
 
 
 const routes: Routes = [
@@ -7,10 +9,13 @@ const routes: Routes = [
   { path: 'contact', loadChildren: () => import('./user/pages/main/contact/contact.module').then(m => m.ContactModule)},
   { path: 'about', loadChildren: () => import('./user/pages/main/about/about.module').then(m => m.AboutModule)},
   { path: 'policy', loadChildren: () => import('./user/pages/guide/policy/policy.module').then(m => m.PolicyModule)},
-  { path: 'chat', loadChildren: () => import('./user/pages/chat/read-chat/read-chat.module').then(m => m.ReadChatModule)},
-  { path: 'register', loadChildren: () => import('./user/auth/register/register.module').then(m => m.RegisterModule)},
+  { path: 'chat',
+  canActivate: [AuthorizedGuard] , loadChildren: () => import('./user/pages/chat/read-chat/read-chat.module').then(m => m.ReadChatModule)},
+  { path: 'register',
+  canActivate: [UnauthorizedGuard] , loadChildren: () => import('./user/auth/register/register.module').then(m => m.RegisterModule)},
   // { path: 'register', loadChildren: () => import('./user/auth/register/register.module').then(m => m.RegisterModule)},
-  { path: 'login', loadChildren: () => import('./user/auth/login/login.module').then(m => m.LoginModule)},
+  { path: 'login',
+  canActivate: [UnauthorizedGuard] , loadChildren: () => import('./user/auth/login/login.module').then(m => m.LoginModule)},
  
   { path: '**', loadChildren: () => import('./user/pages/404/page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)},
   // {path:'shownotes', component:ShowNotesComponent,
